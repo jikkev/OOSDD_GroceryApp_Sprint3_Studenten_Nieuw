c799304bec1d5142dd211aed4e0eb4768b7ae7bd
jikkev <s1198343@student.windesheim.nl>
2025-09-26 15:03:40 +0200

    knop aanmelden gemaakt

--- Grocery.App/Views/GroceryListItemsView.xaml.cs
+++ Grocery.App/Views/GroceryListItemsView.xaml.cs
@@ -1,12 +1,13 @@
 using Grocery.App.ViewModels;
+using Microsoft.Maui.Controls;
 
 namespace Grocery.App.Views;
 
 public partial class GroceryListItemsView : ContentPage
 {
-	public GroceryListItemsView(GroceryListItemsViewModel viewModel)
-	{
-		InitializeComponent();
+    public GroceryListItemsView(GroceryListItemsViewModel viewModel)
+    {
+        InitializeComponent();
         BindingContext = viewModel;
     }
 }
